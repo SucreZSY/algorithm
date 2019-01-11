f1d337c640142e51542d7f7d622b90ce12165e47
SucreZSY <SucreZSY@163.com>
2019-01-11 17:53:30 +0800

    增加了查找任意数组排序后最大间隔的算法，用的是分桶的思想

--- src/com/sucre/algorithm/utils/ArrayUtils.java
+++ src/com/sucre/algorithm/utils/ArrayUtils.java
@@ -12,4 +12,20 @@ public class ArrayUtils {
         arr[indexA] = arr[indexB];
         arr[indexB] = temp;
     }
+
+    /**
+     * 找到数组中最大和最小的数
+     * @param arr 目标数组
+     * @return 返回最大数和最小数的数组，坐标0为最大，坐标1为最小
+     */
+
+    public static int[] findEndpoints(int[] arr){
+        int max = arr[0];
+        int min = max;
+        for (int i : arr) {
+            max = max < i ? i : max;
+            min = min > i ? i : min;
+        }
+        return new int[]{max, min};
+    }
 }
