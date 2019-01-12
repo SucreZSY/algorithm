d20b1b348ed1ae594effc569aec78320fc3484e8
SucreZSY <SucreZSY@163.com>
2019-01-12 23:40:08 +0800

    增加了使用两个栈记录当前栈中最小元素的方法

---
+++ src/com/sucre/algorithm/medium_problem/MinStack.java
@@ -0,0 +1,47 @@
+package com.sucre.algorithm.medium_problem;
+
+import java.util.Stack;
+
+public class MinStack {
+    private Stack<Integer> realStack = new Stack<>();
+    private Stack<Integer> minStack = new Stack<>();
+
+    public boolean push(int element){
+        realStack.push(element);
+        if (minStack.size() == 0){
+            minStack.push(element);
+            return true;
+        }
+        Integer min = minStack.peek();
+        Integer e = min <= element ? minStack.push(min) : minStack.push(element);
+        if (e != null) return true;
+        return false;
+    }
+    public int pop(){
+        minStack.pop();
+        return realStack.pop();
+    }
+    public int peek(){
+        return realStack.peek();
+    }
+    public int getMin(){
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(4);
+        minStack.push(7);
+        System.out.println(minStack.getMin());
+        minStack.push(5);
+        System.out.println(minStack.getMin());
+        minStack.push(3);
+        minStack.push(2);
+        System.out.println(minStack.getMin());
+        minStack.push(4);
+        minStack.push(-1);
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.getMin());
+    }
+}
