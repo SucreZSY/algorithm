df120fc0338cdd23445a24095268b3acc22d3d7f
SucreWP <peng.wu@magnumwm.com>
2023-02-17 18:44:13 +0800

    剑指offer 中等动态规划

--- src/main/java/com/sucre/lc/Code_054.java
+++ src/main/java/com/sucre/lc/Code_054.java
@@ -1,5 +1,7 @@
 package com.sucre.lc;
 
+import com.alibaba.fastjson.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +11,7 @@ import java.util.List;
  * @date 2023/2/16
  */
 public class Code_054 {
+    List<Integer> result;
     /**
      * 给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
      *
@@ -35,7 +38,68 @@ public class Code_054 {
         //四个角分别是(0,0) (0,jLength - 1), (iLength - 1, jLength - 1), (iLength - 1, 0)
         int maxI = matrix.length - 1;
         int maxJ = matrix[0].length - 1;
+        result = new ArrayList<>();
+        int startI = 0;
+        int endI = maxI;
+        int startJ = 0;
+        int endJ = maxJ;
 
-        return new ArrayList<>();
+        while (startI <= endI && startJ <= endJ) {
+            spiralOuterOrder(matrix, startI, endI, startJ, endJ);
+            startI++;
+            endI--;
+            startJ++;
+            endJ--;
+        }
+
+        return result;
     }
+
+    /**
+     * 打印外圈
+     * @param matrix
+     * @param startI
+     * @param endI
+     * @param startJ
+     * @param endJ
+     */
+    public void spiralOuterOrder(int[][] matrix, int startI, int endI, int startJ, int endJ) {
+        int i = startI;
+        int j = startJ;
+        if (startI == endI) {//只有一行 直接打印完即可
+            while (j <= endJ) {
+                result.add(matrix[i][j++]);
+            }
+            return;
+        }
+        if (startJ == endJ) {//只打印一列
+            while (i <= endI) {
+                result.add(matrix[i++][j]);
+            }
+            return;
+        }
+
+        while (j < endJ) {
+            result.add(matrix[i][j++]);
+        }
+        while (i < endI) {
+            result.add(matrix[i++][j]);
+        }
+        while (j > startJ) {
+            result.add(matrix[i][j--]);
+        }
+        while (i > startI) {
+            result.add(matrix[i--][j]);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][] {{1}};
+
+        Code_054 code_054 = new Code_054();
+        code_054.spiralOuterOrder(matrix, 0, 0, 0, 0);
+        System.out.println(JSONObject.toJSONString(code_054.result));
+    }
+
 }
